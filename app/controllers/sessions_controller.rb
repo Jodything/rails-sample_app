4cba32a208b65daf6b477dcd20f16bdfd6cabb34
Jodything <Jodything@gmail.com>
2014-10-28 11:31:57 -0400

    test login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,10 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-
 
   def destroy
   end
